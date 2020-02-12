8a3d988144b5c71a3a6b4ecfa6a0de7d0aefdae1
Matthieu Rosinski <korrigan@slash16.org>
2020-02-12 16:53:23 +0100

    updated BOOMR params

--- plugins/zzz-last-plugin.js
+++ plugins/zzz-last-plugin.js
@@ -1,13 +1,19 @@
 // This code is run after all plugins have initialized
 BOOMR.init({
-  beacon_url: "http://localhost:4242/woidfaosdf092wefwef2cweafawf/beacon.gif",
+  beacon_url: "http://localhost:5200/9c1633d1d083e3f7fc0c7e86a0ba1e23f3f67f6a80eafe10bd/beacon.gif",
+  beacon_type: 'GET',
   instrument_xhr: true,
+  beacon_disable_sendbeacon: true,
   autorun: false,
   AutoXHR: {
-    monitorFetch: true
+    monitorFetch: true,
   },
   History: {
     auto: true
+  },
+  RT: {
+    cookie: '_qta_rum',
+    cookie_exp: 1800, // 30mn
   }
 });
 BOOMR.t_end = new Date().getTime();
