9598a69d0dae770c31e185827d2912d2fb539896
Matthieu Rosinski <korrigan@slash16.org>
2020-02-12 10:41:20 +0100

    added config in zzz-last-plugin

--- plugins/zzz-last-plugin.js
+++ plugins/zzz-last-plugin.js
@@ -1,2 +1,13 @@
 // This code is run after all plugins have initialized
+BOOMR.init({
+  beacon_url: "http://localhost:4242/woidfaosdf092wefwef2cweafawf/beacon.gif",
+  instrument_xhr: true,
+  autorun: false,
+  AutoXHR: {
+    monitorFetch: true
+  },
+  History: {
+    auto: true
+  }
+});
 BOOMR.t_end = new Date().getTime();
