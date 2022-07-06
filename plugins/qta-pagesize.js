0e2c68dc70cc79e0517daafa696d43ffcb11e210
Matthieu Rosinski <korrigan@slash16.org>
2022-07-06 16:10:10 +0200

    handle SPA soft navigations dom count metric in qta-pagesize

--- plugins/qta-pagesize.js
+++ plugins/qta-pagesize.js
@@ -6,16 +6,22 @@
     init: function() {
       this.initialized = true;
       this.records = []
+      this.lastDomCount = 0;
       this.observer = new PerformanceObserver(this.performanceObserverCallback.bind(this));
       this.observer.observe({type: 'resource', buffered: true});
     },
 
     // Called before beacon, we add our parameters to beacon
     done: function() {
+      if (BOOMR.getVar('http.initiator') == 'interaction') {
+        // We do not want to add data or tamper with buffered records for interaction beacons
+        return;
+      }
       var size = 0;
       var count = 0;
       var tao_filtered_count = 0;
-      for (const entry of this.records) {
+      for (var i = 0; i < this.records.length; i++) {
+        var entry = this.records[i];
         if (!entry.transferSize || entry.transferSize <= 0) {
           tao_filtered_count += 1;
         }
@@ -25,15 +31,24 @@
       BOOMR.addVar("qps.fcnt", tao_filtered_count);
       BOOMR.addVar("qps.cnt", count);
       BOOMR.addVar("qps.sz", size);
-      BOOMR.addVar("qps.domcnt", document.getElementsByTagName('*').length); // TODO! how to deal with SPAs ?
 
+      var lastDomCount = this.lastDomCount || 0;
+      this.lastDomCount = document.getElementsByTagName('*').length;
+      if (BOOMR.getVar('http.initiator') == 'spa') {
+        // This is "soft navigation" beacon, we want the diff with the last beacon
+        BOOMR.addVar("qps.domcnt", Math.max(this.lastDomCount - lastDomCount, 0));
+      } else {
+        // This is a hard navigation beacon kind, send the current dom count
+        BOOMR.addVar("qps.domcnt", this.lastDomCount);
+      }
       this.records = []; // We flush our records queue
     },
 
 
     performanceObserverCallback: function(records) {
-      for (const entry of records.getEntries()) {
-        this.records.push(entry);
+      var entries = records.getEntries();
+      for (var i = 0; i < entries.length; i++) {
+        this.records.push(entries[i]);
       }
     },
 
@@ -42,7 +57,7 @@
       BOOMR.removeVar("qps.cnt");
       BOOMR.removeVar("qps.fcnt");
       BOOMR.removeVar("qps.sz");
-      BOOMR.removeVar("qps.domsz");
+      BOOMR.removeVar("qps.domcnt");
     },
   };
 
